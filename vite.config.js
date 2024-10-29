04417d4ca201ec699b883e44cf96fdfc3ecd58f3
imDemian <cdemian79@gmail.com>
2024-10-28 23:23:28 -0600

    añadido config para render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    // eslint-disable-next-line no-undef
+    port: process.env.PORT || 3000,
+  },
 });
